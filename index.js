d7ed046e20540565d4ac9ec7e31bb673523f0e7a
adithya <adithya@example.com>
2021-08-21 19:40:55 +0530

    Index.js updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port =  8080;
 
 app.get('/',(req,res)=>{
-    res.send('express-beanstalk-app is running');
+    res.send('express-beanstalk-app is running updated version');
 });
 app.listen(port,()=>{
     console.log('Listening on port '+port);
